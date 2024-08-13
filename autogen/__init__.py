058e0360fcb7946caf3c9323632aca1a245b73b9
2020-qqtcg <mj1921761583@gmail.com>
2024-08-13 15:45:46 +0800

    fix: autogen export

--- autogen/__init__.py
+++ autogen/__init__.py
@@ -1,9 +1,21 @@
 import logging
 
-# from .agentchat import *
-from .code_utils import DEFAULT_MODEL, FAST_MODEL
+from .agentchat import (
+    ConversableAgent,
+    UserProxyAgent,
+)
+
+from .code_utils import (
+    DEFAULT_MODEL,
+    FAST_MODEL
+)
+
 # from .exception_utils import *
-# from .oai import *
+
+from .oai import (
+    config_list_from_json
+)
+
 from .version import __version__
 
 # Set the root logger.
@@ -13,5 +25,8 @@ logger.setLevel(logging.INFO)
 __all__ = [
     '__version__',
     'DEFAULT_MODEL',
-    'FAST_MODEL'
+    'FAST_MODEL',
+    'ConversableAgent',
+    'UserProxyAgent',
+    'config_list_from_json'
 ]
